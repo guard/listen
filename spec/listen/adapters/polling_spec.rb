cde7298f57966e3463663193097f499f83eecf4b
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2012-01-28 23:16:48 +0100

    Improve polling adapter spec for JRuby

--- spec/listen/adapters/polling_spec.rb
+++ spec/listen/adapters/polling_spec.rb
@@ -7,18 +7,18 @@ describe Listen::Adapters::Polling do
 
     it "calls listener.on_change" do
       adapter = Listen::Adapters::Polling.new(listener)
-      listener.should_receive(:on_change).with(listener.directory)
+      listener.should_receive(:on_change).at_least(1).times.with(listener.directory)
       Thread.new { adapter.start }
-      sleep 0.001
+      sleep 0.1
       adapter.stop
     end
 
     it "calls listener.on_change continuously" do
       adapter = Listen::Adapters::Polling.new(listener)
       adapter.latency = 0.001
-      listener.should_receive(:on_change).exactly(7).times.with(listener.directory)
+      listener.should_receive(:on_change).at_least(10).times.with(listener.directory)
       Thread.new { adapter.start }
-      sleep 0.007
+      sleep 0.1
       adapter.stop
     end
 
