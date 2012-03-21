8596a887b44673ac888fbc29605e7c8fb011d581
Maher Sallam <maher@sallam.me>
2012-03-21 15:25:16 +0100

    Allow the polling adapter to watch multiple directories

--- spec/listen/adapters/polling_spec.rb
+++ spec/listen/adapters/polling_spec.rb
@@ -9,31 +9,58 @@ describe Listen::Adapters::Polling do
     end
   end
 
-  describe "#poll" do
-    let(:listener) { mock(Listen::Listener)}
+  describe '#poll' do
+    let(:listener) { mock(Listen::Listener) }
     let(:callback) { lambda { |changed_dirs, options| @called = true; listener.on_change(changed_dirs, options) } }
-    subject { Listen::Adapters::Polling.new('dir', {}, &callback) }
 
     after { subject.stop }
 
-    it "calls listener.on_change" do
-      listener.should_receive(:on_change).at_least(1).times.with(['dir'], :recursive => true)
-      subject.start
-      subject.wait_for_callback
+    context 'with one directory to watch' do
+      subject { Listen::Adapters::Polling.new('dir', {}, &callback) }
+
+      it 'calls listener.on_change' do
+        listener.should_receive(:on_change).at_least(1).times.with(['dir'], :recursive => true)
+        subject.start
+        subject.wait_for_callback
+      end
+
+      it 'calls listener.on_change continuously' do
+        subject.latency = 0.001
+        listener.should_receive(:on_change).at_least(10).times.with(['dir'], :recursive => true)
+        subject.start
+        10.times { subject.wait_for_callback }
+      end
+
+      it "doesn't call listener.on_change if paused" do
+        subject.paused = true
+        subject.start
+        subject.wait_for_callback
+        @called.should be_nil
+      end
     end
 
-    it "calls listener.on_change continuously" do
-      subject.latency = 0.001
-      listener.should_receive(:on_change).at_least(10).times.with(['dir'], :recursive => true)
-      subject.start
-      10.times { subject.wait_for_callback }
-    end
+    context 'with multiple directories to watch' do
+      subject { Listen::Adapters::Polling.new(%w{dir1 dir2}, {}, &callback) }
 
-    it "doesn't call listener.on_change if paused" do
-      subject.paused = true
-      subject.start
-      subject.wait_for_callback
-      @called.should be_nil
+      it 'calls listener.on_change' do
+        listener.should_receive(:on_change).at_least(1).times.with(%w{dir1 dir2}, :recursive => true)
+        subject.start
+        subject.wait_for_callback
+      end
+
+      it 'calls listener.on_change continuously' do
+        subject.latency = 0.001
+        listener.should_receive(:on_change).at_least(10).times.with(%w{dir1 dir2}, :recursive => true)
+        subject.start
+        10.times { subject.wait_for_callback }
+      end
+
+      it "doesn't call listener.on_change if paused" do
+        subject.paused = true
+        subject.start
+        subject.wait_for_callback
+        @called.should be_nil
+      end
     end
   end
 end
