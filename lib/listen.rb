48d5787020cd604c298bf407d6ff6afab0ee0765
Maher Sallam <maher@sallam.me>
2012-03-26 16:54:10 +0200

    Remove Listen.to_each() in favor of using Listen.to() to handle single and mulitple directories.

--- lib/listen.rb
+++ lib/listen.rb
@@ -13,9 +13,10 @@ module Listen
     autoload :Polling, 'listen/adapters/polling'
   end
 
-  # Listens to filesystem modifications on a single directory.
+  # Listens to filesystem modifications on a either single directory or multiple directories.
   #
   # @param (see Listen::Listener#new)
+  # @param (see Listen::MultiListener#new)
   #
   # @yield [modified, added, removed] the changed files
   # @yieldparam [Array<String>] modified the list of modified files
@@ -25,23 +26,12 @@ module Listen
   # @return [Listen::Listener] the file listener if no block given
   #
   def self.to(*args, &block)
-    listener = Listener.new(*args, &block)
-    block ? listener.start : listener
-  end
+    listener = if args.length == 1 || ! args[1].is_a?(String)
+      Listener.new(*args, &block)
+    else
+      MultiListener.new(*args, &block)
+    end
 
-  # Listens to filesystem modifications on multiple directories.
-  #
-  # @param (see Listen::MultiListener#new)
-  #
-  # @yield [modified, added, removed] the changed files
-  # @yieldparam [Array<String>] modified the list of modified files
-  # @yieldparam [Array<String>] added the list of added files
-  # @yieldparam [Array<String>] removed the list of removed files
-  #
-  # @return [Listen::MultiListener] the file listener if no block given
-  #
-  def self.to_each(*args, &block)
-    listener = MultiListener.new(*args, &block)
     block ? listener.start : listener
   end
 
