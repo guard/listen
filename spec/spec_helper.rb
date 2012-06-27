32b009ad56dded34587ca140a9562abfb919759d
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2012-06-27 21:36:00 +0200

    Randomize specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,9 +6,11 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  config.color_enabled = true
+  config.order = :random
+  config.filter_run :focus => true
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
-  config.filter_run :focus
 end
 
 def test_latency
