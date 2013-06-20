133f07922350026da9ca591b596caca7c80dbd20
Thibaud Guillaume-Gentil <thibaud@thibaud.me>
2013-06-20 21:36:59 +0200

    Skip Celluloid::ActorProxy in spec on jruby

--- spec/support/actor_helper.rb
+++ spec/support/actor_helper.rb
@@ -2,10 +2,12 @@ RSpec.configuration.before(:each) do
   Celluloid.logger = nil
   Celluloid.shutdown
   Celluloid.boot
-  class Celluloid::ActorProxy
-    unless @rspec_compatible
-      @rspec_compatible = true
-      undef_method :should_receive
+  if RUBY_PLATFORM != "java"
+    class Celluloid::ActorProxy
+      unless @rspec_compatible
+        @rspec_compatible = true
+        undef_method :should_receive
+      end
     end
   end
 end
