d96d03b947693dd7fd18b9bd4a908e00c1ec3ba9
Jamie Lawrence <jamie@ideasasylum.com>
2014-02-19 16:25:14 +0000

    Add a listen script to forward events over the network
    Added a `listen` script and a basic CLI (using Thor, like Guard)
    interface. This will forward (-f) the filesystem events to a network
    address, from a directory (-d), and optionally print some verbose
    output (-v)

---
+++ lib/listen/cli.rb
@@ -0,0 +1,68 @@
+require 'thor'
+require 'Listen'
+
+module Listen
+  class CLI < Thor
+    default_task :start
+
+    desc 'start', 'Starts Listen'
+
+    class_option :verbose,
+                  type:    :boolean,
+                  default: false,
+                  aliases: '-v',
+                  banner:  'Verbose'
+
+    class_option :forward,
+                  type:    :string,
+                  default: '127.0.0.1:4000',
+                  aliases: '-f',
+                  banner:  'The address to forward filesystem events'
+
+    class_option :directory,
+                  type:    :string,
+                  default: '.',
+                  aliases: '-d',
+                  banner:  'The directory to listen to'
+
+
+  def start
+    Listen::Forwarder.new(options).start
+  end
+
+  end
+
+  class Forwarder
+    def initialize options
+      @options = options
+    end
+
+    def start
+      puts "Starting listen..."
+      address = @options[:forward]
+      directory = @options[:directory]
+      callback = Proc.new do |modified, added, removed|
+        if @options[:verbose]
+          puts "+ #{added}" unless added.empty?
+          puts "- #{removed}" unless removed.empty?
+          puts "> #{modified}" unless modified.empty?
+        end
+      end
+
+      @listener = Listen.to directory, forward_to: address, &callback
+      @listener.start
+
+      if Signal.list.keys.include?('INT')
+        Signal.trap('INT') do
+          puts "Stopping listen..."
+          @listener.stop
+          exit
+        end
+      end
+
+      while @listener.listen?
+        sleep 0.5
+      end
+    end
+  end
+end
