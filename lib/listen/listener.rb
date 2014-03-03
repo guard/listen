2ab3f9c447f00bdb511a922eb76f4f8ebb47adc3
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2014-03-03 08:45:26 +0100

    Debug travis

--- lib/listen/listener.rb
+++ lib/listen/listener.rb
@@ -145,6 +145,7 @@ module Listen
         sleep options[:wait_for_delay]
       end
 
+      p supervisor
       supervisor.finalize if supervisor.alive?
     rescue => ex
       Kernel.warn "[Listen warning]: Change block raised an exception: #{$!}"
