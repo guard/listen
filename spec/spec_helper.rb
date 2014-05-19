8dd5d2bb8cd7ef585a2154f6e77dbba67adb246c
Cezary Baginski <cezary@chronomantic.net>
2014-05-19 23:13:40 +0200

    bump rspec to 3.0.0rc1

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,10 +16,8 @@ Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-  config.color_enabled = true
   config.order = :random
   config.filter_run focus: true
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.fail_fast = !ci?
   config.expect_with :rspec do |c|
