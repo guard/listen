113b63284b5b53e4777b4dabc4aa46283a5d678a
Cezary Baginski <cezary@chronomantic.net>
2014-05-19 20:50:21 +0200

    fix failing specs (WDM)

--- lib/listen/adapter/windows.rb
+++ lib/listen/adapter/windows.rb
@@ -38,15 +38,65 @@ module Listen
       def _init_worker
         WDM::Monitor.new.tap do |worker|
           _directories_path.each do |path|
-            worker.watch_recursively(path, &_worker_callback)
+            worker.watch_recursively(path.to_s, :files,
+                                     &_worker_file_callback)
+
+            worker.watch_recursively(path.to_s, :directories,
+                                     &_worker_dir_callback)
+
+            worker.watch_recursively(path.to_s, :attributes, :last_write,
+                                     &_worker_attr_callback)
           end
         end
       end
 
-      def _worker_callback
+      def _worker_file_callback
         lambda do |change|
-          options = { type: 'File', change: _change(change.type) }
-          _notify_change(_path(change.path), options)
+          begin
+            path = _path(change.path)
+            options = { type: 'File', change: _change(change.type) }
+            _notify_change(path, options)
+          rescue
+            _log :error, "wdm - callback failed: #{$!}:#{$@.join("\n")}"
+            raise
+          end
+        end
+      end
+
+      def _worker_attr_callback
+        lambda do |change|
+          begin
+            path = _path(change.path)
+            return if path.directory?
+
+            options = { type: 'File', change: _change(change.type) }
+            _notify_change(_path(change.path), options)
+          rescue
+            _log :error, "wdm - callback failed: #{$!}:#{$@.join("\n")}"
+            raise
+          end
+        end
+      end
+
+      def _worker_dir_callback
+        lambda do |change|
+          begin
+            path = _path(change.path)
+            if change.type == :removed
+              _notify_change(path.dirname, type: 'Dir')
+            elsif change.type == :added
+              _notify_change(path, type: 'Dir')
+            else
+              # do nothing - changed directory means either:
+              #   - removed subdirs (handled above)
+              #   - added subdirs (handled above)
+              #   - removed files (handled by _worker_file_callback)
+              #   - added files (handled by _worker_file_callback)
+            end
+          rescue
+            _log :error, "wdm - callback failed: #{$!}:#{$@.join("\n")}"
+            raise
+          end
         end
       end
 
@@ -61,6 +111,10 @@ module Listen
           return change if types.include?(type)
         end
         nil
+      end
+
+      def _log(type, message)
+        Celluloid.logger.send(type, message)
       end
     end
   end
