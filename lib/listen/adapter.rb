1ee33607384139e0a69d8d3866ae4de134630177
Cezary Baginski <cezary@chronomantic.net>
2014-05-19 20:47:57 +0200

    add temporary debug logging (for wdm)

--- lib/listen/adapter.rb
+++ lib/listen/adapter.rb
@@ -12,12 +12,18 @@ module Listen
       'Learn more at https://github.com/guard/listen#polling-fallback.'
 
     def self.select(options = {})
+      _log :debug, 'Adapter: considering TCP ...'
       return TCP if options[:force_tcp]
+      _log :debug, 'Adapter: considering polling ...'
       return Polling if options[:force_polling]
+      _log :debug, 'Adapter: considering optimized backend...'
       return _usable_adapter_class if _usable_adapter_class
-
+      _log :debug, 'Adapter: falling back to polling...'
       _warn_polling_fallback(options)
       Polling
+    rescue
+      _log :warn, "Adapter: failed: #{$!.inspect}:#{$@.join("\n")}"
+      raise
     end
 
     private
@@ -30,6 +36,10 @@ module Listen
     def self._warn_polling_fallback(options)
       msg = options.fetch(:polling_fallback_message, POLLING_FALLBACK_MESSAGE)
       Kernel.warn "[Listen warning]:\n  #{msg}" if msg
+    end
+
+    def self._log(type, message)
+      Celluloid.logger.send(type, message)
     end
   end
 end
