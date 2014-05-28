2fbe515ed521fd76f61c946f9285d36acdd94c8b
Cezary Baginski <cezary@chronomantic.net>
2014-05-28 03:06:28 +0200

    fix+rework OSX specs and implementation

--- lib/listen/file.rb
+++ lib/listen/file.rb
@@ -17,19 +17,21 @@ module Listen
         return :modified
       end
 
-      if data[:mtime] > record_data[:mtime]
+      if data[:mtime] != record_data[:mtime]
         record.async.set_path(:file, path, data)
         return :modified
       end
 
       unless /1|true/ =~ ENV['LISTEN_GEM_DISABLE_HASHING']
-        # On Darwin comparing mtime because of the file mtime second precision.
-        if RbConfig::CONFIG['target_os'] =~ /darwin/i
+        if self.inaccurate_mac_time?(lstat)
           if data[:mtime].to_i == Time.now.to_i
-            md5 = Digest::MD5.file(path).digest
-            if md5 != record_data[:md5]
+            begin
+              md5 = Digest::MD5.file(path).digest
               record.async.set_path(:file, path, data.merge(md5: md5))
-              :modified
+              :modified if record_data[:md5] && md5 != record_data[:md5]
+
+            rescue SystemCallError
+              # ignore failed md5
             end
           end
         end
@@ -40,6 +42,15 @@ module Listen
     rescue
       Celluloid::Logger.debug "lstat failed for: #{path} (#{$!})"
       raise
+    end
+
+    def self.inaccurate_mac_time?(stat)
+      # 'mac' means Modified/Accessed/Created
+
+      # Since precision depends on mounted FS (e.g. you can have a FAT partiion
+      # mounted on Linux), check for fields with a remainder to detect this
+
+      [stat.mtime, stat.ctime, stat.atime].map(&:usec).all?(&:zero?)
     end
   end
 end
