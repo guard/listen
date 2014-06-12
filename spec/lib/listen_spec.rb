6f36681fca7a6997bf304812fe26921588bf6370
Cezary Baginski <cezary@chronomantic.net>
2014-06-13 00:56:39 +0200

    added proper specs for Listen.stop

--- spec/lib/listen_spec.rb
+++ spec/lib/listen_spec.rb
@@ -1,16 +1,22 @@
 require 'spec_helper'
 
 describe Listen do
+  let(:listener) { instance_double(Listen::Listener, stop: nil) }
+
+  after do
+    Listen.stop
+  end
+
   describe '.to' do
     it 'initalizes listener' do
-      expect(Listen::Listener).to receive(:new).with('/path')
+      expect(Listen::Listener).to receive(:new).with('/path') { listener }
       described_class.to('/path')
     end
 
     context 'when using :forward_to option' do
       it 'initializes TCP-listener in broadcast-mode' do
         expect(Listen::Listener).to receive(:new).
-          with(4000, :broadcaster, '/path', {})
+          with(4000, :broadcaster, '/path', {}) { listener }
         described_class.to('/path', forward_to: 4000)
       end
     end
@@ -18,6 +24,9 @@ describe Listen do
 
   describe '.stop' do
     it 'stops all listeners & Celluloid' do
+      allow(Listen::Listener).to receive(:new).with('/path') { listener }
+      expect(listener).to receive(:stop)
+      described_class.to('/path')
       Listen.stop
 
       # TODO: running? returns internal_pool on 0.15.2
@@ -36,7 +45,7 @@ describe Listen do
   describe '.on' do
     it 'initializes TCP-listener in recipient-mode' do
       expect(Listen::Listener).to receive(:new).
-        with(4000, :recipient, '/path')
+        with(4000, :recipient, '/path') { listener }
       described_class.on(4000, '/path')
     end
   end
