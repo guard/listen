666c2bb34515e2b054582b923528583937624cf7
Cezary Baginski <cezary@chronomantic.net>
2015-06-14 22:33:54 +0200

    rework Adapter to use Record and Config

--- lib/listen/adapter/base.rb
+++ lib/listen/adapter/base.rb
@@ -1,4 +1,6 @@
 require 'listen/options'
+require 'listen/record'
+require 'listen/change'
 
 module Listen
   module Adapter
@@ -8,23 +10,18 @@ module Listen
       # TODO: only used by tests
       DEFAULTS = {}
 
-      def initialize(opts)
+      attr_reader :config
+
+      def initialize(config)
+        @started = false
+        @config = config
+
         @configured = nil
-        options = opts.dup
-        @mq = options.delete(:mq)
-        @directories = options.delete(:directories)
 
-        Array(@directories).each do |dir|
-          next if dir.is_a?(Pathname)
-          fail ArgumentError, "not a Pathname: #{dir.inspect}"
-        end
-
-        # TODO: actually use this in every adapter
-        @recursion = options.delete(:recursion)
-        @recursion = true if @recursion.nil?
+        fail 'No directories to watch!' if config.directories.empty?
 
         defaults = self.class.const_get('DEFAULTS')
-        @options = Listen::Options.new(options, defaults)
+        @options = Listen::Options.new(config.adapter_options, defaults)
       rescue
         _log_exception 'adapter config failed: %s:%s called from: %s', caller
         raise
@@ -32,41 +29,60 @@ module Listen
 
       # TODO: it's a separate method as a temporary workaround for tests
       def configure
-        return if @configured
+        if @configured
+          _log(:warn, 'Adapter already configured!')
+          return
+        end
+
         @configured = true
 
         @callbacks ||= {}
-        @directories.each do |dir|
-          unless dir.is_a?(Pathname)
-            fail ArgumentError, "not a Pathname: #{dir.inspect}"
-          end
-
+        config.directories.each do |dir|
           callback = @callbacks[dir] || lambda do |event|
             _process_event(dir, event)
           end
           @callbacks[dir] = callback
           _configure(dir, &callback)
         end
+
+        @snapshots ||= {}
+        # TODO: separate config per directory (some day maybe)
+        change_config = Change::Config.new(config.queue, config.silencer)
+        config.directories.each do |dir|
+          record = Record.new(dir)
+          snapshot = Change.new(change_config, record)
+          @snapshots[dir] = snapshot
+        end
+      end
+
+      def started?
+        @started
       end
 
       def start
         configure
+
+        if started?
+          _log(:warn, 'Adapter already started!')
+          return
+        end
+
+        @started = true
+
         calling_stack = caller.dup
         Listen::Internals::ThreadPool.add do
           begin
+            @snapshots.values.each do |snapshot|
+              _timed('Record.build()') { snapshot.record.build }
+            end
             _run
           rescue
-            msg = "run() in thread failed: %s:\n"\
-              " %s\n\ncalled from:\n %s", calling_stack
-
-            _log_exception(msg)
-            raise
+            msg = 'run() in thread failed: %s:\n'\
+              ' %s\n\ncalled from:\n %s'
+            _log_exception(msg, calling_stack)
+            raise # for unit tests mostly
           end
         end
-      end
-
-      def self.local_fs?
-        true
       end
 
       def self.usable?
@@ -75,9 +91,20 @@ module Listen
 
       private
 
+      def _timed(title)
+        start = Time.now.to_f
+        yield
+        diff = Time.now.to_f - start
+        Listen::Logger.info format('%s: %.05f seconds', title, diff)
+      rescue
+        Listen::Logger.warn "#{title} crashed: #{$ERROR_INFO.inspect}"
+        raise
+      end
+
+      # TODO: allow backend adapters to pass specific invalidation objects
+      # e.g. Darwin -> DirRescan, INotify -> MoveScan, etc.
       def _queue_change(type, dir, rel_path, options)
-        # TODO: temporary workaround to remove dependency on Change
-        @mq.send(:_queue_raw_change, type, dir, rel_path, options)
+        @snapshots[dir].invalidate(type, rel_path, options)
       end
 
       def _log(*args, &block)
@@ -85,20 +112,18 @@ module Listen
       end
 
       def _log_exception(msg, caller_stack)
-        _log :error, format(
+        formatted = format(
           msg,
           $ERROR_INFO,
           $ERROR_POSITION * "\n",
           caller_stack * "\n"
         )
+
+        _log(:error, formatted)
       end
 
       def self._log(*args, &block)
-        if block
-          Listen::Logger.send(*args, block.call)
-        else
-          Listen::Logger.send(*args)
-        end
+        Listen::Logger.send(*args, &block)
       end
     end
   end
