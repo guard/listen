509664d2b713b9c9d1e03ab524461bac923bf6cf
unknown <fwmc34@FWMC34-01.ds.mot.com>
2015-06-29 19:35:36 +0530

    fixed ruby-1.9.3 parser issue for rdoc creation

--- lib/listen/logger.rb
+++ lib/listen/logger.rb
@@ -23,7 +23,7 @@ module Listen
   end
 
   class Logger
-    %i(fatal error warn info debug).each do |meth|
+    [:fatal, :error, :warn, :info, :debug].each do |meth|
       define_singleton_method(meth) do |*args, &block|
         Listen.logger.public_send(meth, *args, &block) if Listen.logger
       end
