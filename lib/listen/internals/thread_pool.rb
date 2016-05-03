e86dccf33327477ef67d658a4533313cedb6f594
Cezary Baginski <cezary@chronomantic.net>
2016-05-03 03:10:04 +0200

    avoid unkillable thread problem on JRuby
    You can't kill a thread that is doing a sysread, so we just send a kill
    to those threads, and then we ignore them.
    
    The allows at least the Travis JRuby tests to run without hanging.

--- lib/listen/internals/thread_pool.rb
+++ lib/listen/internals/thread_pool.rb
@@ -13,8 +13,16 @@ module Listen
         return if @threads.empty? # return to avoid using possibly stubbed Queue
 
         killed = Queue.new
+        # You can't kill a read on a descriptor in JRuby, so let's just
+        # ignore running threads (listen rb-inotify waiting for disk activity
+        # before closing)  pray threads die faster than they are created...
+        limit = RUBY_ENGINE == 'jruby' ? [1] : []
+
         killed << @threads.pop.kill until @threads.empty?
-        killed.pop.join until killed.empty?
+        until killed.empty?
+          th = killed.pop
+          th.join(*limit) unless th[:listen_blocking_read_thread]
+        end
       end
     end
   end
