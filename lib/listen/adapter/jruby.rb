27c157a6bbc27521846e8ddcfa392f191bbd01b6
James Mead <james@floehopper.org>
2019-08-07 14:35:30 +0100

    WIP: Richer calls to _queue_change

--- lib/listen/adapter/jruby.rb
+++ lib/listen/adapter/jruby.rb
@@ -14,17 +14,17 @@ module Listen
         java_import 'java.nio.file.Paths'
         java_import 'java.nio.file.StandardWatchEventKinds'
 
-        event_kinds = [
-          StandardWatchEventKinds::ENTRY_CREATE,
-          StandardWatchEventKinds::ENTRY_MODIFY,
-          StandardWatchEventKinds::ENTRY_DELETE
-        ]
+        @event_kind_map ||= {
+          StandardWatchEventKinds::ENTRY_CREATE => :added,
+          StandardWatchEventKinds::ENTRY_MODIFY => :modified,
+          StandardWatchEventKinds::ENTRY_DELETE => :removed
+        }
 
         @watcher ||= FileSystems.getDefault.newWatchService
         p @watcher.class.name
         @keys ||= {}
         path = Paths.get(directory.to_s)
-        key = path.register(@watcher, *event_kinds)
+        key = path.register(@watcher, *@event_kind_map.keys)
         @keys[key] = path
       end
 
@@ -38,9 +38,17 @@ module Listen
               next if kind == StandardWatchEventKinds::OVERFLOW
               name = event.context
               child = dir.resolve(name)
-p child.to_s
-              pathname = Pathname.new(child.to_s).dirname
-              _queue_change(:dir, pathname, '.', recursive: true)
+              dirname = Pathname.new(child.to_s).dirname
+              full_path = Pathname.new(child.to_s)
+              if full_path.directory?
+                p [:dir, dirname]
+                _queue_change(:dir, dirname, '.', recursive: true)
+              elsif full_path.exist?
+                path = full_path.relative_path_from(dirname)
+                changed = @event_kind_map[kind]
+                p [:file, dirname, path.to_s, changed: changed]
+                _queue_change(:file, dirname, path.to_s, changed: changed)
+              end
             end
           end
           valid = key.reset
