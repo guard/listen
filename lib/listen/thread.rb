8675ef55968ae9e5ba2f0d1e8d697e23c4c7d401
Colin Kelley <colindkelley@gmail.com>
2020-11-04 20:13:27 -0800

    issue #505: add Listen::Thread.rescue_and_log with tests

--- lib/listen/thread.rb
+++ lib/listen/thread.rb
@@ -9,26 +9,31 @@ module Listen
     class << self
       # Creates a new thread with the given name.
       # Any exceptions raised by the thread will be logged with the thread name and complete backtrace.
-      def new(name)
+      def new(name, &block)
         thread_name = "listen-#{name}"
-
         caller_stack = caller
+
         ::Thread.new do
-          begin
-            yield
-          rescue Exception => ex
-            _log_exception(ex, thread_name, caller_stack)
-            nil
-          end
+          rescue_and_log(thread_name, caller_stack: caller_stack, &block)
         end.tap do |thread|
           thread.name = thread_name
         end
       end
 
+      def rescue_and_log(method_name, *args, caller_stack: nil)
+        yield *args
+      rescue Exception => ex
+        _log_exception(ex, method_name, caller_stack: caller_stack)
+      end
+
       private
 
-      def _log_exception(ex, thread_name, caller_stack)
-        complete_backtrace = [*ex.backtrace, "--- Thread.new ---", *caller_stack]
+      def _log_exception(ex, thread_name, caller_stack: nil)
+        complete_backtrace = if caller_stack
+                               [*ex.backtrace, "--- Thread.new ---", *caller_stack]
+                             else
+                               ex.backtrace
+                             end
         message = "Exception rescued in #{thread_name}:\n#{_exception_with_causes(ex)}\n#{complete_backtrace * "\n"}"
         Listen.logger.error(message)
       end
--- spec/lib/listen/thread_spec.rb
+++ spec/lib/listen/thread_spec.rb
@@ -3,56 +3,94 @@
 require 'listen/thread'
 
 RSpec.describe Listen::Thread do
-  let(:name) { "worker_thread" }
-  let(:block) { -> { } }
-  subject { described_class.new(name, &block) }
-
-  it "calls Thread.new" do
-    expect(Thread).to receive(:new) do
-      thread = instance_double(Thread, "thread")
-      expect(thread).to receive(:name=).with("listen-#{name}")
-      thread
-    end
-    subject
-  end
-
-  context "when exception raised" do
-    let(:block) do
-      -> { raise ArgumentError, 'boom!' }
-    end
-
-    it "rescues and logs exceptions" do
-      expect(Listen.logger).to receive(:error)
-        .with(/Exception rescued in listen-worker_thread:\nArgumentError: boom!\n.*\/listen\/thread_spec\.rb/)
-      subject.join
-    end
-
-    it "rescues and logs backtrace + exception backtrace" do
-      expect(Listen.logger).to receive(:error)
-        .with(/Exception rescued in listen-worker_thread:\nArgumentError: boom!\n.*\/listen\/thread\.rb.*--- Thread.new ---.*\/listen\/thread_spec\.rb/m)
-      subject.join
-    end
-  end
-
-  context "when nested exceptions raised" do
-    let(:block) do
-      -> do
+  let(:raise_nested_exception_block) do
+    -> do
+      begin
         begin
-          begin
-            raise ArgumentError, 'boom!'
-          rescue
-            raise 'nested inner'
-          end
+          raise ArgumentError, 'boom!'
         rescue
-          raise 'nested outer'
+          raise 'nested inner'
         end
+      rescue
+        raise 'nested outer'
+      end
+    end
+  end
+
+  let(:raise_script_error_block) do
+    -> do
+      raise ScriptError, "ruby typo!"
+    end
+  end
+
+  describe '.new' do
+    let(:name) { "worker_thread" }
+    let(:block) { -> { } }
+    subject { described_class.new(name, &block) }
+
+    it "calls Thread.new" do
+      expect(Thread).to receive(:new) do
+        thread = instance_double(Thread, "thread")
+        expect(thread).to receive(:name=).with("listen-#{name}")
+        thread
+      end
+      subject
+    end
+
+    context "when exception raised" do
+      let(:block) do
+        -> { raise ArgumentError, 'boom!' }
+      end
+
+      it "rescues and logs exceptions" do
+        expect(Listen.logger).to receive(:error)
+          .with(/Exception rescued in listen-worker_thread:\nArgumentError: boom!\n.*\/listen\/thread_spec\.rb/)
+        subject.join
+      end
+
+      it "rescues and logs backtrace + exception backtrace" do
+        expect(Listen.logger).to receive(:error)
+          .with(/Exception rescued in listen-worker_thread:\nArgumentError: boom!\n.*\/listen\/thread\.rb.*--- Thread.new ---.*\/listen\/thread_spec\.rb/m)
+        subject.join
       end
     end
 
-    it "details exception causes" do
+    context "when nested exceptions raised" do
+      let(:block) { raise_nested_exception_block }
+
+      it "details exception causes" do
+        expect(Listen.logger).to receive(:error)
+          .with(/RuntimeError: nested outer\n--- Caused by: ---\nRuntimeError: nested inner\n--- Caused by: ---\nArgumentError: boom!/)
+        subject.join
+      end
+    end
+
+    context 'when exception raised that is not derived from StandardError' do
+      let(:block) { raise_script_error_block }
+
+      it "still rescues and logs" do
+        expect(Listen.logger).to receive(:error).with(/Exception rescued in listen-worker_thread:\nScriptError: ruby typo!/)
+        subject.join
+      end
+    end
+  end
+
+  describe '.rescue_and_log' do
+    it 'rescues and logs nested exceptions' do
       expect(Listen.logger).to receive(:error)
-        .with(/RuntimeError: nested outer\n--- Caused by: ---\nRuntimeError: nested inner\n--- Caused by: ---\nArgumentError: boom!/)
-      subject.join
+       .with(/Exception rescued in method:\nRuntimeError: nested outer\n--- Caused by: ---\nRuntimeError: nested inner\n--- Caused by: ---\nArgumentError: boom!/) do |message|
+        expect(message).to_not match(/Thread\.new/)
+      end
+      described_class.rescue_and_log("method", &raise_nested_exception_block)
+    end
+
+    context 'when exception raised that is not derived from StandardError' do
+      let(:block) { raise_script_error_block }
+
+      it 'still rescues and logs' do
+        expect(Listen.logger).to receive(:error).with(/Exception rescued in method:\nScriptError: ruby typo!/)
+        described_class.rescue_and_log("method", &block)
+      end
     end
   end
 end
