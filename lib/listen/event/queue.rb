c6fe53b160f000a8d51a5b6c9d2ed6a4bba6e9ff
Colin Kelley <colindkelley@gmail.com>
2020-11-23 16:22:31 -0800

    issue #504: remove early returns and private attr_readers

--- lib/listen/event/queue.rb
+++ lib/listen/event/queue.rb
@@ -30,20 +30,21 @@ module Listen
         fail "Invalid change: #{change.inspect}" unless change.is_a?(Symbol)
         fail "Invalid path: #{path.inspect}" unless path.is_a?(String)
 
-        dir = _safe_relative_from_cwd(dir)
-        event_queue.public_send(:<<, [type, change, dir, path, options])
+        dir = if @config.relative?
+                _safe_relative_from_cwd(dir)
+              else
+                dir
+              end
+        @event_queue << [type, change, dir, path, options]
       end
 
-      delegate empty?: :event_queue
-      delegate pop: :event_queue
-      delegate close: :event_queue
+      delegate empty?: :@event_queue
+      delegate pop: :@event_queue
+      delegate close: :@event_queue
 
       private
 
-      attr_reader :event_queue, :config
-
       def _safe_relative_from_cwd(dir)
-        return dir unless config.relative?
         dir.relative_path_from(Pathname.pwd)
       rescue ArgumentError
         dir
